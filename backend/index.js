30c03dd6d58353858c8525f19e118d8070cf5f34
HamzaKhan001 <spaceus9@gmail.com>
2023-05-30 00:24:30 +0500

    server file

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import helmet from 'helmet'
+import morgan from 'morgan'
+
+
+dotenv.config();
+const app=express();
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({policy: 'cross-origin'}));
+app.use(morgan('common'))
