ed5b305de2146e8a1e93e0b5da249b574bc22676
HamzaKhan001 <spaceus9@gmail.com>
2023-05-30 00:40:58 +0500

    server settings

--- backend/index.js
+++ backend/index.js
@@ -12,4 +12,12 @@ const app=express();
 app.use(express.json())
 app.use(helmet())
 app.use(helmet.crossOriginResourcePolicy({policy: 'cross-origin'}));
-app.use(morgan('common'))
+app.use(morgan('common'));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(cors())
+
+
+// Routes
+
+
