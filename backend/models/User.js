92806c2942a29927b9a4817e569c028b76c3e1e1
HamzaKhan001 <spaceus9@gmail.com>
2023-06-03 01:38:15 +0500

    user schema

---
+++ backend/models/User.js
@@ -0,0 +1,46 @@
+import mongoose, { mongo } from 'mongoose'
+
+
+const UserSchema=new mongoose.UserSchema({
+	name:{
+		type:String,
+		required:true,
+		min:2,
+		max:100
+	},
+	email:{
+		type:String,
+		required:true,
+		min:50,
+		unique:true
+	},
+	password:{
+		type:String,
+		required:true,
+		min:8,
+	},
+	city:{
+		type:String
+	},
+	state:{
+		type:String
+	},
+	country:{
+		type:String
+	},
+	occupation:{
+		type:String
+	},
+	phoneNumber:{
+		type:String
+	},
+	transactions:Array,
+	role:{
+		type:String,
+		enum:['user','admin','superadmin'],
+		default:'admin'
+	}
+
+}, {
+	timestamps:true
+})
