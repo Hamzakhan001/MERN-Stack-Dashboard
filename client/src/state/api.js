aadfab21b1eb71a29082c3f9802ce8c315ccca03
HamzaKhan001 <spaceus9@gmail.com>
2023-06-03 12:26:42 +0500

    redux toolkit query

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -6,5 +6,13 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 export const api= createApi({
 	baseQuery:fetchBaseQuery({baseUrl:process.env.REACT_APP_BASE_URL}),
 	reducerPath:"adminApi",
-	tagTypes:["User"]
+	tagTypes:["User"],
+	endpoints:(build)=>({
+		getUser:build.query({
+			query:(id)=> `general/user/${id}`,
+		})
+	})
 })
+
+
+export const{useGetUserQuery}= api;
