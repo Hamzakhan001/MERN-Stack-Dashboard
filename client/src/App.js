8363702581aee07e8c388a9d582972f8fd14be83
HamzaKhan001 <spaceus9@gmail.com>
2023-06-04 15:14:57 +0500

    products frontend

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import { useMemo } from "react";
 import {BrowserRouter, Route, Routes,Navigate} from 'react-router-dom'
 import Dashboard from "./scenes/dashboard";
 import Layout from "./scenes/layout";
+import Products from "./scenes/products"
 
 function App() {
   const mode=useSelector((state)=>state.global.mode)
@@ -21,6 +22,7 @@ function App() {
           <Route element={<Layout/>}>
             <Route path="/" element={<Navigate to="/dashboard" replace/>} />
             <Route path="/dashboard" element={ <Dashboard/> } />
+            <Route path="/products" element={ <Products/> } />
 
           </Route>
         </Routes>
---
+++ client/src/scenes/products/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Box, card, CardActions, CardContent, Collapse, Button, Typography} from '@mui/material';
+import { Header } from '../../components/Header';
+import {useGetProductsQuery} from './state/api'
+
+const index = () => {
+  return (
+	<Box>
+		<Header title="" />
+	</Box>
+  )
+}
+
+export default index
