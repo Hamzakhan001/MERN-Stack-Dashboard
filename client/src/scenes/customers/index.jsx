b2202e9b3a870dccf4bde89360ef19130deb5e58
HamzaKhan001 <spaceus9@gmail.com>
2023-06-07 22:44:20 +0500

    customers table columns

--- client/src/scenes/customers/index.jsx
+++ client/src/scenes/customers/index.jsx
@@ -16,13 +16,58 @@ const Customers = () => {
 		{
 			field:"_id",
 			headerName:"ID",
-			
-		}
+			flex:1,
+
+		},
+		{
+			field:"name",
+			headerName:"Name",
+			flex:0.5,
+
+		},
+		{
+			field:"_id",
+			headerName:"ID",
+			flex:1,
+
+		},
+		{
+			field:"email",
+			headerName:"Email",
+			flex:1,
+
+		},
+		{
+			field:"phoneNumber",
+			headerName:"Phone Number",
+			flex:0.5,
+			renderCell:(params)=>{
+				return params.value.replace(/^(\d{3})(\d{3})(\d{4})/,"($1)$2-$3")
+			}
+		},
+		{
+			field:"country",
+			headerName:"Country",
+			flex:0.4,
+
+		},
+		{
+			field:"occupation",
+			headerName:"Occupation",
+			flex:1,
+
+		},
+		{
+			field:"role",
+			headerName:"Role",
+			flex:1,
+
+		},
 	]
   return (
 	<Box m="1.5rem 2.5rem">
 		<Header title="CUSTOMERS" subtitle="List of Customers" />
-		<Box>
+		<Box mt="40px" height="75vh">
 			<DataGrid 
 			loading={isDataLoading || !data}
 			getRowId={(row)=> row._id}
