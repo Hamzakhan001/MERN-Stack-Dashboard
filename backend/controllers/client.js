be71a00c54cadf141f5f7cbd032c2a966dc21d77
HamzaKhan001 <spaceus9@gmail.com>
2023-06-08 01:57:31 +0500

    transactions backend with pagination

--- backend/controllers/client.js
+++ backend/controllers/client.js
@@ -1,6 +1,7 @@
 
 import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
+import Transaction from "../models/Transaction.js";
 import User from '../models/User.js'
 
 
@@ -31,6 +32,47 @@ export const getProducts=async(req,res)=>{
 export const getCustomers = async (req,res)=>{
 	try {
 		const customers=await User.find({ role:"user" }).select("-password");
+		res.status(200).json(customers)
+	}
+	catch(err) {
+		console.log("req",req.params)
+		res.status(404).json({'message':err.message})
+	}
+}
+
+export const getTransactions = async (req,res)=>{
+	try {
+
+		const { page=1, pageSize=20, sort=null, search=" " }= req.query;
+		
+		const generateSort = () => {
+			const sortParsed = JSON.parse(sort);
+			const sortFormatted = {
+				[sortParsed.field] : sortParsed.sort = "asc" ? 1: -1
+			};
+
+			return sortFormatted;
+		}
+		const sortFormatted = Boolean(sort) ? generateSort() : {};
+		const transaction = await Transaction.find({
+			$or: [
+				{cost: {$regex: new RegExp(search,"i")}},
+				{userId: {$regex: new RegExp(search, "i")}}
+			]
+		})
+		.sort(sortFormatted)
+		.skip(page * pageSize)
+		.limit(pageSize)
+
+		const total = await Transaction.countDocuments({
+			name : {$regex : search, $options: "i"}
+		})
+
+		res.status(200).json({
+			transaction,
+			total
+		})
+
 		res.status(200).json(customers)
 	}
 	catch(err) {
