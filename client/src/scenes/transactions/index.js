c670412f257cc467c1cb90c64743503bd0f3f47e
HamzaKhan001 <spaceus9@gmail.com>
2023-06-08 02:30:10 +0500

    latest transactions

--- client/src/scenes/transactions/index.js
+++ client/src/scenes/transactions/index.js
@@ -2,8 +2,25 @@ import React,{ useState } from 'react'
 import {DataGrid} from '@mui/x-data-grid';
 import { useGetTransactionsQuery } from '../../state/api';
 import { Header } from '../../components/Header';
+import { useTheme } from '@emotion/react';
 
 const Transactions = () => {
+  const theme = useTheme();
+
+  const [page, setPage] =  useState(0);
+  const [pageSize, setPageSize] = useState(20);
+  const [sort, setSort] = useState({});
+  const [search, setSearch] = useState("");
+
+
+  const { data, isLoading }= useGetTransactionsQuery({
+	page,
+	pageSize,
+	sort : JSON.stringify(sort),
+	search
+  })
+
+	
   return (
 	<div>Transactions</div>
   )
