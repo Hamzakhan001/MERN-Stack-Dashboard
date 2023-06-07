00175000b672fc945d9fafc2f263718616f673e2
HamzaKhan001 <spaceus9@gmail.com>
2023-06-07 21:40:39 +0500

    latest produt/product detail

--- client/src/scenes/products/index.js
+++ client/src/scenes/products/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {Box, card, CardActions, CardContent, Collapse, Button, Typography, useMediaQuery, Card} from '@mui/material';
+import {Box, card, CardActions, CardContent, Collapse, Button, Typography, useMediaQuery, Card, Rating} from '@mui/material';
 import { Header } from '../../components/Header';
 import {useGetProductsQuery} from './state/api'
 import { useTheme } from '@emotion/react';
@@ -17,10 +17,39 @@ const Product=({
 		borderRadius:"0.55rem"
 	}}>
 		<CardContent>
-			<Typography sx={{fontSize:14}} color={theme.palette.secondary[700]} gutterBottom>
-			
+			<Typography sx={{fontSize:14}} color={theme.palette.secondary[700]} gutterBottom >
+				{category}
 			</Typography>
+			<Typography variant="h5" component="div">
+				{category}
+			</Typography>
+			<Typography sx={{mb:"1.5rem"}} color={theme.palette.secondary[400]} gutterBottom >
+				${Number(price).toFixed(3)}
+			</Typography>
+			<Rating value={rating} readOnly />
+			<Typography variant='body2'>{description}</Typography>
 		</CardContent>
+		<CardActions>
+			<Button variant='primary' size="small" onClick={()=> setIsExpanded(!isExpanded)}>
+				See More
+			</Button>
+		</CardActions>
+		<Collapse
+		in={isExpanded}
+		timeout="auto"
+		unmountOnExit
+		sx={{
+			color:theme.palette.neutral[300]
+
+		}}
+		>
+			<CardContent>
+				<Typography>id : {_id}</Typography>
+				<Typography>Supply : {supply}</Typography>
+				<Typography>Yearly sales this year  : {stat.yearlySalesTotal}</Typography>
+				<Typography>Yearly unis sold this year  : {stat.yearlyTotalSoldUnits}</Typography>
+			</CardContent>
+		</Collapse>
 	</Card>
 }
 
@@ -41,9 +70,20 @@ const Products = () => {
 			// 	"& > div": {gridColumn ? isNonMobile ? undefined : "span 4"}
 			// }}
 			>
-				{data.map ()=>{
-
-				}}
+				{
+					data.map(({_id,name,description,price,rating,category,supply,stat})=>(
+						<Product
+					     id={_id}
+						 name={name}
+						 description={description}
+						 price={price}
+						 rating={rating}
+						 category={category}
+						 supply={supply}
+						 stat={stat}
+					/>
+					))
+				}
 			</Box>
 		):<>Loading ...</> }
 	</Box>
