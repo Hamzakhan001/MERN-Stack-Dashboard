0437a43430c9380a27957d4bc973d21966ccab86
HamzaKhan001 <spaceus9@gmail.com>
2023-06-08 02:18:12 +0500

    redux toolkit query api call with params

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -18,8 +18,15 @@ export const api= createApi({
 		getCustomers:build.query({
 			query:()=> `client/customers`,
 			providesTags:["Customers"]
+		}),
+		getTransactions:build.query({
+			query:({page, pageSize, sort, search })=> ({
+				url: "client/transactions",
+				method: "GET",
+				params: { page, pageSize, sort, search }
+			}),
+			
 		})
-
 	})
 })
 
