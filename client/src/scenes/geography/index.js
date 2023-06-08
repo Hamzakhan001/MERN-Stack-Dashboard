ce67295b7489f480370c8363edd4c9b57a973c08
HamzaKhan001 <spaceus9@gmail.com>
2023-06-09 01:39:00 +0500

    gis data chart using nivo

--- client/src/scenes/geography/index.js
+++ client/src/scenes/geography/index.js
@@ -2,10 +2,29 @@ import React from 'react'
 import {Box,useTheme } from '@mui/material';
 import { useGetGeographyQuery } from '../../state/api';
 import Header from '../../components/Header';
+import {ResponsiveChoropleth} from '@nivo/geo';
+import { gisdata } from '../../state/gisData';
 
 const Geography = () => {
+
+	const theme = useTheme();
+	const {data} = useGetGeographyQuery();
+
+
+
   return (
-	<div>Geography</div>
+	<Box m="1.5rem 2.5rem">
+		<Header title="GEOGRAPHY" subtitle="Find out where users are located"/>
+		<Box 
+		mt="40px"
+		height="75vh"
+		border={`1px solid ${theme.palette.secondary[200]}`}
+		borderRadius="4px"
+		>
+			{data ? (<></>) : <>Loading ...</>}
+
+		</Box>
+	</Box>
   )
 }
 
