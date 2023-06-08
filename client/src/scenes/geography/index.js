026f822a19c93edcadae0cbf2520ed2bee5b9db1
HamzaKhan001 <spaceus9@gmail.com>
2023-06-09 01:19:38 +0500

    geography route added

---
+++ client/src/scenes/geography/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Box,useTheme } from '@mui/material';
+import { useGetGeographyQuery } from '../../state/api';
+import Header from '../../components/Header';
+
+const Geography = () => {
+  return (
+	<div>Geography</div>
+  )
+}
+
+export default Geography
