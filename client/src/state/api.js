5fed9619cdfdd210ddc7126c55d64a4d73d2aa5f
HamzaKhan001 <spaceus9@gmail.com>
2023-06-09 01:14:36 +0500

    geography frontend

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -6,7 +6,7 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 export const api= createApi({
 	baseQuery:fetchBaseQuery({baseUrl:process.env.REACT_APP_BASE_URL}),
 	reducerPath:"adminApi",
-	tagTypes:["User","Products","Customers","Transactions"],
+	tagTypes:["User","Products","Customers","Transactions","Geography"],
 	endpoints:(build)=>({
 		getUser:build.query({
 			query:(id)=> `general/user/${id}`,
@@ -26,9 +26,13 @@ export const api= createApi({
 				params: { page, pageSize, sort, search }
 			}),
 			providesTags:["Transactions"]
+		}),
+		getGeography:build.query({
+			query:()=> "client/geography",
+			providesTags:["Geography"]
 		})
 	})
 })
 
 
-export const{ useGetUserQuery,useGetProductsQuery,useGetCustomersQuery,useGetTransactionsQuery }= api;
+export const{ useGetUserQuery,useGetProductsQuery,useGetCustomersQuery,useGetTransactionsQuery,useGetGeographyQuery }= api;
