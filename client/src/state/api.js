9f933af8ee79931d2aeca23684c256a2da2edc2a
HamzaKhan001 <spaceus9@gmail.com>
2023-06-10 04:56:42 +0500

    backend model+controller+route for stats frontend

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -6,7 +6,7 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 export const api= createApi({
 	baseQuery:fetchBaseQuery({baseUrl:process.env.REACT_APP_BASE_URL}),
 	reducerPath:"adminApi",
-	tagTypes:["User","Products","Customers","Transactions","Geography"],
+	tagTypes:["User","Products","Customers","Transactions","Geography","Sales"],
 	endpoints:(build)=>({
 		getUser:build.query({
 			query:(id)=> `general/user/${id}`,
@@ -30,6 +30,10 @@ export const api= createApi({
 		getGeography:build.query({
 			query:()=> "client/geography",
 			providesTags:["Geography"]
+		}),
+		getSales:build.query({
+			query : () => "sales/sales",
+			providesTags : ["Sales"]
 		})
 	})
 })
