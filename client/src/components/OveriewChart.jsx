b16cbb7d26c4b61e1dec6eb5d956a1b4ea6a2d61
HamzaKhan001 <spaceus9@gmail.com>
2023-06-10 11:24:37 +0500

    overview chart update

--- client/src/components/OveriewChart.jsx
+++ client/src/components/OveriewChart.jsx
@@ -1,10 +1,34 @@
-import React,{useState} from 'react';
+import React,{useMemo} from 'react';
 import {ResponsiveLine} from "@nivo/line";
 import { useTheme } from '@mui/material';
 import { useGetSalesQuery } from '../state/api';
 
 
 const OveriewChart = ({ isDashboard =false, view }) => {
+	const theme = useTheme();
+	const { data,isLoading } = useGetSalesQuery();
+	const [totalSalesLine, totalUnitsLine] =useMemo (()=>{
+		if(!data) return [];
+
+		const {monthlyData} = data;
+		const totalSalesLine ={
+			id:"totalSales",
+			color: theme.palette.secondary.main,
+			data :[]
+		};
+
+		const totalUnitsLine = {
+			id:"totalUnits",
+			color:theme.palette.secondary[600],
+			data:[],
+		}
+		Object.values(monthlyData).reduce((acc ,{month,totalSales, totalUnits}) =>{
+			
+		})
+	},[data])
+
+
+
   return (
 	<div>OveriewChart</div>
   )
